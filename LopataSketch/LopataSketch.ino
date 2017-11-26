8f837d0625c7289616924e4daf57e1aa25c99ea3
Олег Моисеев < >
2017-11-26 14:56:00 +0300

    Add project files.

---
+++ LopataSketch/LopataSketch.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		LopataSketch.ino
+ Created:	11/26/2017 2:55:53 PM
+ Author:	oleg
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
